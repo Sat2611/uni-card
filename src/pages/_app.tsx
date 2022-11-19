ecc626c4cf81f6821330464970e6d651f3a42583
Satyam Dahlan <dahlansatyam@gmail.com>
2022-11-19 11:01:28 +0530

    push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
-import { DataProvider } from "../context/DataProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="font-['Quicksand']">
       <Layout>
-        {/* <DataProvider> */}
         <Component {...pageProps} />
-        {/* </DataProvider> */}
       </Layout>
     </div>
   );
